8ee3ea6afaf7c46ab118684988520e9db01c11b5
unknown <nhatnsm1225@gmail.com>
2025-05-21 11:25:19 +0700

    add

---
+++ WebsiteBanHang/Controllers/CategoryController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebsiteBanHang.Models;
+using WebsiteBanHang.Repositories;
+
+namespace WebsiteBanHang.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryController(ICategoryRepository context)
+        {
+            _categoryRepository = context;
+        }
+
+        // GET: Category/Add
+        public IActionResult Index()
+        {
+            var categories = _categoryRepository.GetAllCategories;
+            return View(categories);
+        }
+        public IActionResult Display(int id)
+        {
+            var categories = _categoryRepository.GetById(id);
+            if (categories == null)
+            {
+                return NotFound();
+            }
+            return View(categories);
+        }
+
+        // POST: Category/Add
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Add(Category category)
+        {
+                _categoryRepository.Add(category);
+                return RedirectToAction("Index");
+        
+
+            var categories = _categoryRepository.GetAllCategories();
+            ViewBag.Categories = new SelectList(categories, "Id", "Name", category.Id);
+            return View(categories);
+        }
+
+    }
+}
---
+++ WebsiteBanHang/Repositories/MockCategoryRepository.cs
@@ -0,0 +1,52 @@
+﻿using WebsiteBanHang.Models;
+
+namespace WebsiteBanHang.Repositories
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        private List<Category> _categoryList;
+        public MockCategoryRepository()
+        {
+            _categoryList = new List<Category>
+        {
+         new Category { Id = 1, Name = "Laptop" },
+         new Category { Id = 2, Name = "Điện Thoại" },
+         // Thêm các category khác
+
+           };
+        }
+        public IEnumerable<Category> GetAllCategories()
+        {
+            return _categoryList;
+        }
+
+        public Category GetById(int id)
+        {
+            return _categoryList.FirstOrDefault((p => p.Id == id));
+        }
+
+        public void Add(Category category)
+        {
+            category.Id = _categoryList.Max(p => p.Id) + 1;
+            _categoryList.Add(category);
+        }
+
+        public void Update(Category category)
+        {
+            var index = _categoryList.FindIndex(p => p.Id == category.Id);
+            if (index != -1)
+            {
+                _categoryList[index] = category;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var category = _categoryList.FirstOrDefault(p => p.Id == id);
+            if (category != null)
+            {
+                _categoryList.Remove(category);
+            }
+        }
+    }
+}
